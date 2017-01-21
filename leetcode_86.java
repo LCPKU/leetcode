01bea3002e5d2206b33f93582bfab48e47a1f66f
刘畅 <292301526@qq.com>
2017-01-21 16:37:19 +0800

    Update leetcode_86.java

--- leetcode_86.java
+++ leetcode_86.java
@@ -1,5 +1,24 @@
 package leetcode;
 
 public class leetcode_86 {
+      public ListNode partition(ListNode head, int x) {
+        ListNode p = new ListNode(0);
+        ListNode pHead = p;
+        ListNode q = new ListNode(0);
+        ListNode qHead = q;
+        while(head != null){
+            if(head.val < x){
+                p.next = head;
+                p = p.next;
+            }else{
+                q.next = head;
+                q = q.next;
+            }
+            head = head.next;
+        }
+        p.next = qHead.next;
+        q.next = null;
+        return pHead.next;
+    }
 
 }
