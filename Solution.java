b70af5487585b8dfe581a5a0c17b47ba4274b130
刘畅 <292301526@qq.com>
2017-01-23 21:58:20 +0800

    leetcode 15

--- Solution.java
+++ Solution.java
@@ -1,29 +1,65 @@
-
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
 public class Solution {
-    public String intToRoman(int num) {
-        String str="";
-        int count=0;
-        String [] str1={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        int [] value={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        while(num!=0)
-        {
-        	if(num>=value[count])
-        	{
-        		num=num-value[count];
-        		str=str+str1[count];
-        	}
-        	else
-        	{
-        		count++;
-        	}
-        }
-        return str;
+	List<List<Integer>> ret = new ArrayList<List<Integer>>();
+    public List<List<Integer>> threeSum(int[] nums) {
+    if (nums == null || nums.length < 3) return ret; 
+   // List<List<Integer>> ret = new ArrayList<List<Integer>>();//¶şÎ¬LIST
+    Arrays.sort(nums);
+    for(int i=0;i<nums.length-2;i++)
+    {
+    	//int begin=0;
+    	//int end=nums.length-1;
+    	if(i>0&&nums[i]==nums[i-1])
+    	{
+    		continue;
+    	}
+    	find(nums,i,i+1,nums.length-1);
+    }
+    return ret;
+    }
+    public void find(int[] nums, int target, int begin, int end)
+    {
+    	
+    	int b=begin;
+    	int e=end;
+    	while(b<e)
+    	{
+    		List<Integer> ans= new ArrayList<Integer>();
+    	if((nums[target]+nums[b]+nums[e])==0)
+    	{
+    		ans.add(nums[target]);
+    		ans.add(nums[b]);
+    		ans.add(nums[e]);
+    		ret.add(ans);
+    		while(b<e&&nums[b]==nums[b+1])
+    		{
+    			b++;
+    		}
+    		while(b<e&&nums[e]==nums[e-1])
+    		{
+    			e--;
+    		}
+    		b++;
+    		e--;
+    	}
+    	else if((nums[target]+nums[b]+nums[e])<0)
+    	{
+    		b++;
+    	}
+    	else
+    	{
+    		e--;
+    	}
+    }
     }
     public static void main(String [] args)
     {
     	Solution s=new Solution();
-    	int n=456;
-    	String w=s.intToRoman(n);
-    	System.out.print(w);
+    	int [] nums={-1,0,1,2,-1,4};
+    	List<List<Integer>> caf = new ArrayList<List<Integer>>();
+    	caf=s.threeSum(nums);
+    	System.out.println(caf);	
     }
 }
