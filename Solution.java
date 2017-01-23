acc47e63372098518da1292d31d9e2682b91e8ed
刘畅 <292301526@qq.com>
2017-01-23 21:58:46 +0800

    leetcode 25

--- Solution.java
+++ Solution.java
@@ -1,65 +1,27 @@
-import java.util.List;
-import java.util.Arrays;
-import java.util.ArrayList;
+
 public class Solution {
-	List<List<Integer>> ret = new ArrayList<List<Integer>>();
-    public List<List<Integer>> threeSum(int[] nums) {
-    if (nums == null || nums.length < 3) return ret; 
-   // List<List<Integer>> ret = new ArrayList<List<Integer>>();//¶şÎ¬LIST
-    Arrays.sort(nums);
-    for(int i=0;i<nums.length-2;i++)
-    {
-    	//int begin=0;
-    	//int end=nums.length-1;
-    	if(i>0&&nums[i]==nums[i-1])
-    	{
-    		continue;
-    	}
-    	find(nums,i,i+1,nums.length-1);
-    }
-    return ret;
-    }
-    public void find(int[] nums, int target, int begin, int end)
-    {
-    	
-    	int b=begin;
-    	int e=end;
-    	while(b<e)
-    	{
-    		List<Integer> ans= new ArrayList<Integer>();
-    	if((nums[target]+nums[b]+nums[e])==0)
-    	{
-    		ans.add(nums[target]);
-    		ans.add(nums[b]);
-    		ans.add(nums[e]);
-    		ret.add(ans);
-    		while(b<e&&nums[b]==nums[b+1])
-    		{
-    			b++;
-    		}
-    		while(b<e&&nums[e]==nums[e-1])
-    		{
-    			e--;
-    		}
-    		b++;
-    		e--;
-    	}
-    	else if((nums[target]+nums[b]+nums[e])<0)
-    	{
-    		b++;
-    	}
-    	else
-    	{
-    		e--;
-    	}
-    }
-    }
-    public static void main(String [] args)
-    {
-    	Solution s=new Solution();
-    	int [] nums={-1,0,1,2,-1,4};
-    	List<List<Integer>> caf = new ArrayList<List<Integer>>();
-    	caf=s.threeSum(nums);
-    	System.out.println(caf);	
-    }
+     public int RemoveDuplicates(int a[])
+	{
+      int n=a.length;
+	  if(n==0)
+	  {
+		  return 0;
+	  }
+	  int j=0;
+	  for(int i=1;i<n;i++)
+	  {
+		  if(a[j]!=a[i])
+		  {
+			  a[++j]=a[i];
+		  }
+	  }
+	  return j+1;  
+	}
+     public static void main(String[] args)
+     {
+    	 int[] a={1,1,2};
+    	 Solution s=new Solution();
+    	 int u=s.RemoveDuplicates(a);
+    	 System.out.println(u);
+     }
 }
